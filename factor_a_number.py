5fdf6165121359c44ec12d5b1f04095c811f8ef4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 14:54:12 -0400

    refactoring

--- factor_a_number.py
+++ factor_a_number.py
@@ -1,19 +1,13 @@
 # given a number, print it's factors
 # get user input for number and set total
-num = int(input("Give me a number, any number: "))
-num_A = 1
-num_B = num
+inp = int(input("Give me a number, any number: ")) / 2 # to reduce loops
+num = 1
 
-while num > 0:
-    if num_A * num_B == num:
-        print(num_A)
-        num_A += 1
-        num_B -= 1
-        num -= 1
+
+while num <= inp:
+    if inp % num == 0:
+        print(num)
+        num += 1
         continue
-    else:
-        num_A += 1
-        num_B -= 1
-        num -= 1 
-        continue 
+
     
