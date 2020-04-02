0f31ca25c2a495792df6d3876c56d9912fce299c
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 14:58:27 -0400

    Completed Factor A Number exercise

--- factor_a_number.py
+++ factor_a_number.py
@@ -1,13 +1,14 @@
 # given a number, print it's factors
 # get user input for number and set total
-inp = int(input("Give me a number, any number: ")) / 2 # to reduce loops
+inp = int(input("Give me a number, any number: ")) 
 num = 1
 
-
+# Logic
 while num <= inp:
-    if inp % num == 0:
+    if inp % num == 0: # if input can be divided evenly by number, it is a factor
         print(num)
-        num += 1
-        continue
-
-    
+        num += 1 # increment num by 1
+        continue # start loop again
+    else: # if input cannot be divided evenly by number, it is not a factor so we do not print
+        num += 1 # increment num by 1
+        continue # start loop again
