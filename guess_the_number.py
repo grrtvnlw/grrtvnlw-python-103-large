66074fbef8008fda984556651251483731f3dcba
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 17:16:38 -0400

    Completed Guess the Number exercise

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,8 +1,6 @@
 # build a guess the number game using user input and giving hints
 # import randint function from random module
 from random import randint
-# set input variable
-inp = 0
 # set game counter variable, will decrement so guesses left works
 counter = 5
 # make the winning number a random integer 
@@ -10,8 +8,8 @@ winning_num = randint(1, 10)
 
 # Logic
 while counter >= 0:
-    if counter != 0:
-        # prompt the user
+    if counter != 0: # if user still has guesses left
+        # prompt the user for input
         print(f"I am thinking of a number between 1 and 10. \nYou have {counter} guesses left.")
         # update the input variable to hold user input
         inp = int(input("What's the number? "))
@@ -20,7 +18,7 @@ while counter >= 0:
             play_again = input("Do you want to play again (Y or N)? ")
             if play_again == "Y":
                 counter = 5 # reset counter to 5 
-                # have to reset winning number or else it will be the same
+                # have to reset winning number or else it will be the same as last game
                 winning_num = randint(1, 10)
                 continue
             elif play_again == "N":
@@ -34,11 +32,11 @@ while counter >= 0:
             print(str(inp) + " is too high.") # f-string wouldn't work here
             counter -= 1 
             continue # try again, go back to the while loop
-    elif counter == 0:
+    elif counter == 0: # if user has run out of guesses
         play_again = input("You lose. Do you want to play again (Y or N)? ")
         if play_again == "Y":
             counter = 5 # reset counter to 5 
-            # have to reset winning number or else it will be the same
+            # have to reset winning number or else it will be the same as last game
             winning_num = randint(1, 10)
             continue
         elif play_again == "N":
