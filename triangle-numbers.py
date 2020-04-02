95d4ddf25c93c220be18238e49cfeae909a1dd5f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 13:56:30 -0400

    completed Triangle Numbers

---
+++ triangle-numbers.py
@@ -0,0 +1,9 @@
+# print the first 100 triangle numbers
+# build variables for tri_num and counter 
+counter = 1
+tri_num = 1
+
+while counter <= 100: # run loop 100 times
+    print(tri_num)
+    counter += 1 # increment counter by 1 every time
+    tri_num += counter # increment tri_num by counter
