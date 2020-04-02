e12981f179e2ccce86fed0445c5ffbf7609a02b4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-02 17:12:08 -0400

    Completed Guess the Number exercise

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,18 +1,47 @@
 # build a guess the number game using user input and giving hints
-# hard code the number 5 and save user input
-winning_num = 5
+# import randint function from random module
+from random import randint
+# set input variable
 inp = 0
+# set game counter variable, will decrement so guesses left works
+counter = 5
+# make the winning number a random integer 
+winning_num = randint(1, 10)
 
 # Logic
-while inp != winning_num:
-    print("I am thinking of a number between 1 and 10. ")
-    inp = int(input("What's the number? "))
-    if inp == winning_num:
-        print("Yes! You win!")
-        break # exit the loop
-    elif inp < winning_num:
-        print(str(inp) + " is too low.")
-        continue # try again, go back to the while loop
-    elif inp > winning_num:
-        print(str(inp) + " is too high.")
-        continue # try again, go back to the while loop
+while counter >= 0:
+    if counter != 0:
+        # prompt the user
+        print(f"I am thinking of a number between 1 and 10. \nYou have {counter} guesses left.")
+        # update the input variable to hold user input
+        inp = int(input("What's the number? "))
+        if inp == winning_num: 
+            print("Yes! You win!")
+            play_again = input("Do you want to play again (Y or N)? ")
+            if play_again == "Y":
+                counter = 5 # reset counter to 5 
+                # have to reset winning number or else it will be the same
+                winning_num = randint(1, 10)
+                continue
+            elif play_again == "N":
+                print("Bye!")
+                break # exit the loop
+        elif inp < winning_num:
+            print(str(inp) + " is too low.") # f-string wouldn't work here
+            counter -= 1 
+            continue # try again, go back to the while loop
+        elif inp > winning_num:
+            print(str(inp) + " is too high.") # f-string wouldn't work here
+            counter -= 1 
+            continue # try again, go back to the while loop
+    elif counter == 0:
+        play_again = input("You lose. Do you want to play again (Y or N)? ")
+        if play_again == "Y":
+            counter = 5 # reset counter to 5 
+            # have to reset winning number or else it will be the same
+            winning_num = randint(1, 10)
+            continue
+        elif play_again == "N":
+            print("Bye!")
+            break
+            
