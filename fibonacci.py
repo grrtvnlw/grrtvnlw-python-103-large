9370438cdb238426ada909d40e10a1f0fb625dba
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-03 12:03:43 -0400

    Completed Fibonacci Numbers exercise.

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,6 @@
 # given an input, print out Fibonacci sequence
 # get input from user 
-inp = int(input("Give me a number: "))
+inp = int(input("How many Fibonacci numbers would you like to see? "))
 
 # create counter and number variables / need 2 number variables 
 counter = 0
@@ -8,9 +8,9 @@ num_one = 0
 num_two = 1
 
 # counts the iterations of the loop from zero to input
-while counter <= inp:
+while counter < inp:
     print(num_one) # print the first number
-    next_num = num_one + num_two # make the third number the first number plus the second number
+    num_three = num_one + num_two # make the third number the first number plus the second number
     num_one = num_two # make the first number equal the second number
-    num_two = next_num # make the second number equal the third number
+    num_two = num_three # make the second number equal the third number
     counter += 1 # increment the counter
